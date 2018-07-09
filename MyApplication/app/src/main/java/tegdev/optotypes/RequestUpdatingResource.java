967b1e08b286ea60bb0a1a8b491dae22e9f052e0
edgkid <edglandaeta.15@gmail.com>
2018-07-09 10:38:45 -0430

    Clase para manejar solicitudes de actualización

---
+++ MyApplication/app/src/main/java/tegdev/optotypes/RequestUpdatingResource.java
@@ -0,0 +1,26 @@
+package tegdev.optotypes;
+
+import android.content.Context;
+
+/**
+ * Created by edgar on 09/07/2018.
+ */
+
+public class RequestUpdatingResource {
+
+    private String request;
+    private Context context;
+
+    public RequestUpdatingResource(String request, Context context) {
+        this.request = request;
+        this.context = context;
+    }
+
+    /**
+     * This method allows request data patient to loca uses
+     */
+    public void requestResourcePatient(){
+
+    }
+
+}
