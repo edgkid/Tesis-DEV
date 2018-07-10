e3da59e0360c6d2eb90b2ce7948c32dbf73a7ca8
edgkid <edglandaeta.15@gmail.com>
2018-07-10 10:02:38 -0430

    Clase para manejar hilo en servicio

---
+++ MyApplication/app/src/main/java/tegdev/optotypes/BackGroundServiceForUpdate.java
@@ -0,0 +1,14 @@
+package tegdev.optotypes;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by edgar on 10/07/2018.
+ */
+
+public class BackGroundServiceForUpdate extends AsyncTask<Void, Integer, Boolean> {
+    @Override
+    protected Boolean doInBackground(Void... voids) {
+        return null;
+    }
+}
