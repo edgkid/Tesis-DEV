4e0d8fb4cf926d1ebba17931bc4eb9cfdd147600
edgkid <edglandaeta.15@gmail.com>
2018-07-16 10:08:42 -0400

    Cambio en estructura de datos

--- MyApplication/app/src/main/java/tegdev/optotypes/FormDataDbContract.java
+++ MyApplication/app/src/main/java/tegdev/optotypes/FormDataDbContract.java
@@ -13,38 +13,11 @@ public class FormDataDbContract {
         public static final String TABLE_NAME = "form_db_app";
 
         public static final String ID = "idform";
-        public static final String IDPATIENT = "idPatient";
-        public static final String YEARSOLD = "yearsOld";
-        public static final String GENDER = "gender";
-
-        public static final String AVRIGTH = "avRigth";
-        public static final String AVLEFT = "avLeft";
-
-        public static final String CENTER = "center";
-        public static final String SUSTAIN = "sustain";
-        public static final String MAINTAIN = "maintain";
-
-        public static final String CHRONOMATICOD = "chronomaticOd";
-        public static final String CHRONOMATICOI = "chronomaticOi";
-
-        public static final String TONOMETRIAOD = "tonometriaOd";
-        public static final String TONOMETRIAOI = "tonometria Oi";
-
-        public static final String FORIA = "foria";
-        public static final String ENDOFORIA = "endoforia";
-        public static final String EXOFORIA = "exoforia";
-        public static final String ORTOFORIA = "ortoforia";
-        public static final String ORTOTROPIA = "ortotropia";
-        public static final String DVD = "dvd";
-        public static final String CAELEVADA = "caElevada";
-
-        public static final String ANTECEDENTDAD = "antecedentDad";
-        public static final String ANTECEDENTMON = "antecedentMon";
-        public static final String SIGNALDEFECT = "signalDefect";
-
-        public static final String TYPETEST = "typeTest";
-        public static final String COLABORATEGRADE = "colaborateGrade";
-
+        public static final String PATIENTDATA = "patientData";
+        public static final String AVDATA = "avData";
+        public static final String OTHERTESTA = "otherTestA";
+        public static final String OTHERTESTB = "otherTestB";
+        public static final String TESTUSED = "testUsed";
         public static final String STATUS = "status";
 
     }
--- MyApplication/app/src/main/java/tegdev/optotypes/FormDataDbHelper.java
+++ MyApplication/app/src/main/java/tegdev/optotypes/FormDataDbHelper.java
@@ -1,67 +1,54 @@
 package tegdev.optotypes;
 
-import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
 /**
- * Created by edgar on 15/07/2018.
+ * Created by elandaeta on 16/7/2018.
  */
 
 public class FormDataDbHelper extends DbApp {
+
     public FormDataDbHelper(Context context) {
         super(context);
     }
-
 
     @Override
     public void onCreate(SQLiteDatabase db) {
 
         String sql ="";
-        ContentValues values = new ContentValues();
 
-        // crear Tablas de base de datos
         sql = sql + "CREATE TABLE " + FormDataDbContract.FormDataEntry.TABLE_NAME + " ( ";
         sql = sql + FormDataDbContract.FormDataEntry._ID + " INTEGER PRIMARY KEY, ";
         sql = sql + FormDataDbContract.FormDataEntry.ID + " TEXT, ";
-        sql = sql + FormDataDbContract.FormDataEntry.IDPATIENT + " TEXT, ";
-        sql = sql + FormDataDbContract.FormDataEntry.YEARSOLD + " TEXT, ";
-        sql = sql + FormDataDbContract.FormDataEntry.GENDER + " TEXT, ";
-        sql = sql + FormDataDbContract.FormDataEntry.AVRIGTH + " TEXT, ";
-        sql = sql + FormDataDbContract.FormDataEntry.AVLEFT + " TEXT, ";
-        sql = sql + FormDataDbContract.FormDataEntry.CENTER + " TEXT, ";
-        sql = sql + FormDataDbContract.FormDataEntry.SUSTAIN + " TEXT, ";
-        sql = sql + FormDataDbContract.FormDataEntry.MAINTAIN + " TEXT, ";
-
-        sql = sql + FormDataDbContract.FormDataEntry.CHRONOMATICOD + " TEXT,";
-        sql = sql + FormDataDbContract.FormDataEntry.CHRONOMATICOI + " TEXT, ";
-        sql = sql + FormDataDbContract.FormDataEntry.TONOMETRIAOD + " TEXT, ";
-        sql = sql + FormDataDbContract.FormDataEntry.TONOMETRIAOI + " TEXT, ";
-        sql = sql + FormDataDbContract.FormDataEntry.FORIA + " TEXT, ";
-        sql = sql + FormDataDbContract.FormDataEntry.ENDOFORIA + " TEXT, ";
-        sql = sql + FormDataDbContract.FormDataEntry.EXOFORIA + " TEXT, ";
-        sql = sql + FormDataDbContract.FormDataEntry.ORTOFORIA + " TEXT, ";
-        sql = sql + FormDataDbContract.FormDataEntry.ORTOTROPIA + " TEXT, ";
-        sql = sql + FormDataDbContract.FormDataEntry.DVD + " TEXT, ";
-        sql = sql + FormDataDbContract.FormDataEntry.CAELEVADA + " TEXT, ";
-
+        sql = sql + FormDataDbContract.FormDataEntry.PATIENTDATA + " TEXT, ";
+        sql = sql + FormDataDbContract.FormDataEntry.AVDATA + " TEXT, ";
+        sql = sql + FormDataDbContract.FormDataEntry.OTHERTESTA + " TEXT, ";
+        sql = sql + FormDataDbContract.FormDataEntry.OTHERTESTB + " TEXT, ";
+        sql = sql + FormDataDbContract.FormDataEntry.TESTUSED + " TEXT, ";
+        sql = sql + FormDataDbContract.FormDataEntry.STATUS + " TEXT, ";
 
         sql = sql + " UNIQUE ( " + FormDataDbContract.FormDataEntry.ID + " ) ";
         sql = sql +" ) ";
 
         db.execSQL(sql);
+
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
         String sql ="";
-        ContentValues values = new ContentValues();
 
-        // crear Tablas de base de datos
         sql = sql + "CREATE TABLE " + FormDataDbContract.FormDataEntry.TABLE_NAME + " ( ";
         sql = sql + FormDataDbContract.FormDataEntry._ID + " INTEGER PRIMARY KEY, ";
         sql = sql + FormDataDbContract.FormDataEntry.ID + " TEXT, ";
+        sql = sql + FormDataDbContract.FormDataEntry.PATIENTDATA + " TEXT, ";
+        sql = sql + FormDataDbContract.FormDataEntry.AVDATA + " TEXT, ";
+        sql = sql + FormDataDbContract.FormDataEntry.OTHERTESTA + " TEXT, ";
+        sql = sql + FormDataDbContract.FormDataEntry.OTHERTESTB + " TEXT, ";
+        sql = sql + FormDataDbContract.FormDataEntry.TESTUSED + " TEXT, ";
+        sql = sql + FormDataDbContract.FormDataEntry.STATUS + " TEXT, ";
 
         sql = sql + " UNIQUE ( " + FormDataDbContract.FormDataEntry.ID + " ) ";
         sql = sql +" ) ";
@@ -69,5 +56,4 @@ public class FormDataDbHelper extends DbApp {
         db.execSQL(sql);
 
     }
-
 }
