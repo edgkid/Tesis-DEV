4804925ab2860817cc46d48b71f6519cb902a471
edgkid <edglandaeta.15@gmail.com>
2018-07-28 19:56:58 -0430

    Nuevo diseño Ventana diagnostico

--- MyApplication/app/src/main/java/tegdev/optotypes/DiagnosticActivity.java
+++ MyApplication/app/src/main/java/tegdev/optotypes/DiagnosticActivity.java
@@ -16,111 +16,20 @@ import java.util.ArrayList;
 
 public class DiagnosticActivity extends AppCompatActivity implements View.OnClickListener{
 
-    ImageView imagePhoto;
 
-    TextView textDate;
-    TextView textPatient;
-    TextView textYears;
-    static TextView textAVRE;
-    static TextView textAVLE;
-    static TextView textAVRP;
-    static TextView textAVLP;
-    static TextView textCenter;
-    static TextView textSustain;
-    static TextView textMaintain;
-
-    Button comeBackButton;
-    Bundle patientExtras;
-
-    PatientsToday patientsToday = new PatientsToday();
-    static public ArrayList<Diagnostic> listData = new ArrayList<Diagnostic>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diagnostic);
 
-        imagePhoto = (ImageView) findViewById(R.id.diagnosticPhoto);
-        textDate = (TextView) findViewById(R.id.diagnosticAppointment);
-        textPatient = (TextView) findViewById(R.id.diagnosticPatient);
-        textYears = (TextView) findViewById(R.id.diagnosticYear);
-        textAVRE = (TextView) findViewById(R.id.diagnosticAvRigthE);
-        textAVLE = (TextView) findViewById(R.id.diagnosticAvLeftE);
-        textAVRP = (TextView) findViewById(R.id.diagnosticAvRigthP);
-        textAVLP = (TextView) findViewById(R.id.diagnosticAvLeftP);
-        textCenter = (TextView) findViewById(R.id.diagnosticCenter);
-        textSustain = (TextView) findViewById(R.id.diagnosticSustain);
-        textMaintain = (TextView) findViewById(R.id.diagnosticMaintain);
-        comeBackButton = (Button) findViewById(R.id.diagnosticButton);
-        comeBackButton.setOnClickListener(this);
-
-        Intent intentData = getIntent();
-        patientExtras = intentData.getExtras();
-
-
-        if (patientExtras != null){
-
-            String date = "";
-
-            patientsToday.setIdPatient(Integer.parseInt(patientExtras.getString("idPatient")));
-            patientsToday.setName(patientExtras.getString("patient"));
-            patientsToday.setYearsOld(patientExtras.getString("yearsOld"));
-            Bitmap photo = (Bitmap) patientExtras.get("photo");
-            date = patientExtras.getString("date");
-
-            showData(date, photo);
-
-        }
 
     }
 
     @Override
     public void onClick(View v) {
 
-        switch (v.getId()){
-            case R.id.diagnosticButton:
-                Intent newActivity = new Intent(this, DashBoardActivity.class);
-                startActivity(newActivity);
-                break;
-            case R.id.buttonLogout:
-                //Toast.makeText(this, "LogOut", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.buttonUpdate:
-                //Toast.makeText(this, "Update", Toast.LENGTH_SHORT).show();
-                break;
-        }
-    }
-
-    /**
-     * This method show the patient data in crud read appointment
-     */
-    public void showData(String date, Bitmap photo){
-
-        textDate.setText("Fecha: " + date);
-        textPatient.setText("Paciente: " + patientsToday.getName());
-        textYears.setText(patientsToday.getYearsOld());
-
-        if (photo != null){
-           imagePhoto.setImageBitmap(photo);
-        }else{
-            imagePhoto.setImageResource(R.drawable.usuario_icon);
-        }
-        fillData();
 
     }
-
-    /**
-     * This method fill data in activity
-     */
-    public void fillData (){
-
-        int action = 1;
-
-        RequestDiagnostic requestDiagnostic = new RequestDiagnostic( this);
-        requestDiagnostic.requestAllDataDiagnostic(listData, String.valueOf(patientsToday.getIdPatient()), action);
-
-    }
-
-
 
 }
